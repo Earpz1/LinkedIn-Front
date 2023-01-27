eeebcf5ea1123e5d1f5346dfef98f4ff0e28948e
Daniel <dan.earp@icloud.com>
2023-01-27 21:50:25 +0000

    Initial

---
+++ src/Components/HomePageRight.jsx
@@ -0,0 +1,11 @@
+const HomePageRight = () => {
+  return (
+    <>
+      <div className="right-feed-box ml-3">
+        <h1>Right side content</h1>
+      </div>
+    </>
+  )
+}
+
+export default HomePageRight
